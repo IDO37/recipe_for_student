303eca4cffa784e2ddb199b246e3dc8b967322c6
IDO37 <charles5275407@gmail.com>
2025-07-15 20:19:20 +0900

    up

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://axlospaekpdlhlokvijz.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF4bG9zcGFla3BkbGhsb2t2aWp6Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1MjM4ODIzNCwiZXhwIjoyMDY3OTY0MjM0fQ.bQ0iH94gFa2FvD6MgkRwBW9Pig1uQNDpLp4feAyAS_Y'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey) 
